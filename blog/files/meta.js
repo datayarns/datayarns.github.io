cac2cf0d4506050683624e9297f5cd3701753370
Evangelia Berdou <evangeliaberdou@gmail.com>
2022-11-16 15:57:58 +0000

    fourth

--- blog/files/meta.js
+++ blog/files/meta.js
@@ -56,7 +56,7 @@
 	};
  
 	// Object containing all website meta info
-	var websiteMeta = {"archive-november-2022.html":"Archives for November 2022","9379654f5794964f5492f0e738f7f6d7-0.html":"test"};
+	var websiteMeta = {"archive-november-2022.html":"Archives for November 2022","9d58a8a179ffa160c971aa05a5681c2d-0.html":"Something or another"};
  
 	// pageId must match the key in websiteMeta object
 	var url = window.location.pathname;
